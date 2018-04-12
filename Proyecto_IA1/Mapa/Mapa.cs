4dd51f52c1d4382091597be0617d5436d6e5ba95
Sharketo <Christian0777@hotmail.com>
2018-04-12 02:11:11 -0600

    A estrella funcional
    Falta veificar cuando hay solucion o no, y tambien cambiar tamaño de
    matriz

--- Proyecto_IA1/Mapa/Mapa.cs
+++ Proyecto_IA1/Mapa/Mapa.cs
@@ -10,145 +10,56 @@ namespace Proyecto_IA1.Mapa
     class Mapa
     {
         public int[][] matriz { get; set; }
+        public int[][] distancias_M { get; set; }
+        public double[][] coste_Total { get; set; }
         public int inicioX { get; set; }
         public int inicioY { get; set; }
         public int finalX { get; set; }
         public int finalY { get; set; }
         public int tamañoCuadro { get; set; }
-        public int[] solucion { get; set; }
+        public int[][][] solucion { get; set; }
+        public bool[][] visitados { get; set; }
+        public bool modoDiagonal;
 
         public Mapa(int m, int n, int a)
         {
-            this.matriz = generarMatrizVacia(m, n, 0);
+            this.matriz = generarMatrizVaciaInt(m, n, 0);
+            this.distancias_M = generarMatrizVaciaInt(m, n, 0);
+            this.coste_Total = generarMatrizVaciaDouble(m, n, 0);
             this.tamañoCuadro = a;
-            this.solucion = generarListaInt(m * n, -1);
+            this.solucion = generarMatrizSolucion(m, n, -1, -1);
+            this.visitados = generarMatrizVisitados(m, n);
         }
 
-        private int indiceMatriz_Lista(int x, int y)
+        private int[][][] generarMatrizSolucion(int m, int n, int valorX, int valorY)
         {
-            int n = this.matriz[0].Length;
-            return x * n + y;
-        }
-
-        private int[] indiceLista_Matriz(int num)
-        {
-            int n = this.matriz[0].Length;
-            int[] res = new int[2];
-            res[0] = (num / n);
-            res[1] = (num % n);
+            int[][][] res = new int[m][][];
+            for (int i = 0; i < m; i++)
+            {
+                res[i] = new int[n][];
+                for (int j = 0; j < n; j++)
+                {
+                    int[] coordenada = new int[2];
+                    coordenada[0] = valorX;
+                    coordenada[1] = valorY;
+                    res[i][j] = coordenada;
+                }
+            }
             return res;
         }
 
-        private void dijkstra()
+        private bool[][] generarMatrizVisitados(int m, int n)
         {
-            int m = this.matriz.Length;
-            int n = this.matriz[0].Length;
-
-            double[] distancias = this.generarListaDouble(m * n, -1);
-            int[] visitados = this.generarListaInt(m * n, 0);
-            ArrayList listaVecinos = new ArrayList();
-            listaVecinos.Add(new Nodo(finalX, finalY, finalX, finalY, 0));
-
-            dijkstraAux(distancias, visitados, listaVecinos);
-        }
-
-        private Nodo removerMenor(ArrayList lista, double[] distancias)
-        {
-            int indice = 0;
-            Nodo nodoMenor;
-            for (int i = 0; i < lista.Count; i++)
+            bool[][] res = new bool[m][];
+            for (int i = 0; i < m; i++)
             {
-                Nodo nodo1 = (Nodo)lista[i];
-                Nodo nodo2 = (Nodo)lista[indice];
-                double distanciaAcumulada1 = distancias[indiceMatriz_Lista(nodo1.inicioX, nodo1.inicioY)];
-                double distanciaAcumulada2 = distancias[indiceMatriz_Lista(nodo2.inicioX, nodo2.inicioY)];
-
-                if (nodo1.distancia + distanciaAcumulada1 < nodo2.distancia + distanciaAcumulada2)
+                res[i] = new bool[n];
+                for (int j = 0; j < n; j++)
                 {
-                    indice = i;
+                    res[i][j] = false;
                 }
             }
-
-            nodoMenor = (Nodo)lista[indice];
-            lista.RemoveAt(indice);
-            return nodoMenor;
-        }
-
-
-        private bool esPrimero(Nodo nodo)
-        {
-            if (nodo.inicioX == nodo.finalX && nodo.inicioY == nodo.finalY)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        private void dijkstraAux(double[] distancias, int[] visitados, ArrayList listaVecinos)
-        {
-
-            if (listaVecinos.Count > 0)
-            {
-                Nodo nodoMenor = removerMenor(listaVecinos, distancias);
-                int inicioX = nodoMenor.inicioX;
-                int inicioY = nodoMenor.inicioY;
-                int finalX = nodoMenor.finalX;
-                int finalY = nodoMenor.finalY;
-                double distanciaNodo = nodoMenor.distancia;
-                double distanciaAcumulada1 = distancias[indiceMatriz_Lista(inicioX, inicioY)];
-                double distanciaAcumulada2 = distancias[indiceMatriz_Lista(finalX, finalY)];
-
-                if (distanciaNodo + distanciaAcumulada1 < distanciaAcumulada2 || distanciaAcumulada2 == -1)
-                {
-                    distancias[indiceMatriz_Lista(finalX, finalY)] = distanciaNodo + distanciaAcumulada1;
-                    solucion[indiceMatriz_Lista(finalX, finalY)] = indiceMatriz_Lista(inicioX, inicioY);
-
-                    if (visitados[indiceMatriz_Lista(finalX, finalY)] == 0) //no esta visitado
-                    {
-                        listaVecinos.AddRange(obtenerVecinos(finalX, finalY));
-                        visitados[indiceMatriz_Lista(finalX, finalY)] = 1;
-                    }
-                }
-
-                dijkstraAux(distancias, visitados, listaVecinos);
-            }
-        }
-
-        private ArrayList obtenerVecinos(int x, int y)
-        {
-            ArrayList listaNueva = new ArrayList();
-            int m = this.matriz.Length;
-            int n = this.matriz[0].Length;
-
-            for (int i = -1; i <= 1; i++)
-            {
-                for (int j = -1; j <= 1; j++)
-                {
-                    if (0 <= x + i && x + i < m)
-                    {
-                        if (0 <= y + j && y + j < n)
-                        {
-                            if (this.matriz[x + i][y + j] != 1)
-                            {
-                                double distancia;
-                                if(!(i == 0 && j == 0))
-                                {
-                                    if (i != 0 && j != 0)
-                                    {
-                                        distancia = Math.Sqrt(2) * this.tamañoCuadro;
-                                    }
-                                    else
-                                    {
-                                        distancia = this.tamañoCuadro;
-                                    }
-                                    listaNueva.Add(new Nodo(x, y, x + i, y + j, distancia));
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return listaNueva;
+            return res;
         }
 
         private int[] generarListaInt(int n, int valor)
@@ -171,13 +82,24 @@ namespace Proyecto_IA1.Mapa
             return listaNueva;
         }
 
-        private int[][] generarMatrizVacia(int m, int n, int valor)
+        private int[][] generarMatrizVaciaInt(int m, int n, int valor)
         {
             int[][] matrizNueva = new int[m][];
 
             for (int i = 0; i < m; i++)
             {
                 matrizNueva[i] = generarListaInt(n, valor);
+            }
+            return matrizNueva;
+        }
+
+        private double[][] generarMatrizVaciaDouble(int m, int n, int valor)
+        {
+            double[][] matrizNueva = new double[m][];
+
+            for (int i = 0; i < m; i++)
+            {
+                matrizNueva[i] = generarListaDouble(n, valor);
             }
             return matrizNueva;
         }
@@ -194,15 +116,13 @@ namespace Proyecto_IA1.Mapa
             this.finalY = y;
         }
 
+
         private void colocarObstaculos(int cantObstaculos)
         {
             int m = this.matriz.Length;
             int n = this.matriz[0].Length;
-
             Random rnd = new Random();
-
             int cont = 0;
-
             while (cont < cantObstaculos)
             {
                 int randX = rnd.Next(0, m);
@@ -219,7 +139,172 @@ namespace Proyecto_IA1.Mapa
                     }
                 }
             }
+        }
 
+        private void calcular_distancias_M()
+        {
+            int m = this.matriz.Length;
+            int n = this.matriz[0].Length;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    this.distancias_M[i][j] = Math.Abs(inicioX - i) + Math.Abs(inicioY - j);
+                }
+            }
+        }
+
+        private ArrayList obtenerVecinos(Coordenada c)
+        {
+            int m = this.matriz.Length;
+            int n = this.matriz[0].Length;
+            ArrayList lista = new ArrayList();
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    int x = c.x + i;
+                    int y = c.y + j;
+
+                    if (!(x >= m || x < 0))
+                    {
+                        if (!(y >= n || y < 0))
+                        {
+                            if(this.matriz[x][y] != 1)
+                            {
+                                if (!this.visitados[x][y])
+                                {
+                                    if (i == 0 || j == 0)
+                                    {
+                                        lista.Add(new Coordenada(x, y, tamañoCuadro));
+                                    }
+                                    else
+                                    {
+                                        if (this.modoDiagonal)
+                                        {
+                                            lista.Add(new Coordenada(x, y, tamañoCuadro * Math.Sqrt(2)));
+                                        }
+                                    }
+                                }
+                            } 
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+
+        private int indiceMenor(ArrayList vecinos)
+        {
+            int i = 0;
+            Coordenada primera = (Coordenada)vecinos[0];
+            double costeTotal = this.coste_Total[primera.x][primera.y] + this.distancias_M[primera.x][primera.y];
+            int cont = 0;
+
+            foreach (Coordenada c in vecinos)
+            {
+                double costeActual = this.coste_Total[c.x][c.y] + this.distancias_M[c.x][c.y];
+                if (costeActual < costeTotal)
+                {
+                    i = cont;
+                }
+                cont++;
+            }
+            return i;
+        }
+
+        private Coordenada obtenerMenor(ArrayList vecinos)
+        {
+            int indice = indiceMenor(vecinos);
+            Coordenada c = (Coordenada)vecinos[indice];
+            vecinos.RemoveAt(indice);
+
+            return c;
+        }
+
+        private int indiceFinal(ArrayList vecinos)
+        {
+            int cont = 0;
+            foreach (Coordenada c in vecinos)
+            {
+                if (c.x == inicioX && c.y == inicioY)
+                {
+                    return cont;
+                }
+                cont++;
+            }
+            return -1;
+        }
+
+        private void a_estrella(bool diagonales)
+        {
+            this.modoDiagonal = diagonales;
+            ArrayList vecinos = new ArrayList();
+            Coordenada c = new Coordenada(finalX, finalY, 0);
+            vecinos.Add(c);
+            this.solucion[finalX][finalY][0] = finalX;
+            this.solucion[finalX][finalY][1] = finalY;
+            a_estrella_aux(vecinos, false);
+        }
+
+
+
+        private void a_estrella_aux(ArrayList vecinos, bool termino)
+        {
+
+            bool termino_actual = false;
+            if (!termino)
+            {
+
+                Coordenada c;
+
+                if (indiceFinal(vecinos) != -1)
+                {
+                    c = (Coordenada)vecinos[indiceFinal(vecinos)];
+                    termino_actual = true;
+                } else
+                {
+                    c = obtenerMenor(vecinos);
+                }
+
+                visitados[c.x][c.y] = true;
+
+                ArrayList vecinos_nuevos = obtenerVecinos(c);
+                foreach (Coordenada c_nuevo in vecinos_nuevos)
+                {
+                    if (this.solucion[c_nuevo.x][c_nuevo.y][0] == -1)
+                    {
+                        this.solucion[c_nuevo.x][c_nuevo.y][0] = c.x;
+                        this.solucion[c_nuevo.x][c_nuevo.y][1] = c.y;
+                        vecinos.Add(c_nuevo);
+                    } else
+                    {
+                        if (calcularHeuristica(c_nuevo, c) < calcularHeuristica(c_nuevo))
+                        {
+                            this.solucion[c_nuevo.x][c_nuevo.y][0] = c.x;
+                            this.solucion[c_nuevo.x][c_nuevo.y][1] = c.y;
+
+                            this.coste_Total[c_nuevo.x][c_nuevo.y] = c_nuevo.distancia + this.coste_Total[c.x][c.y];
+                        }
+                    }
+                }
+                a_estrella_aux(vecinos, termino_actual);
+            }
+        }
+
+        private double calcularHeuristica(Coordenada c_actual, Coordenada c_anterior)
+        {
+            return this.distancias_M[c_actual.x][c_actual.y] + this.coste_Total[c_anterior.x][c_anterior.y];
+        }
+
+        private double calcularHeuristica(Coordenada c)
+        {
+            return this.distancias_M[c.x][c.y] + this.coste_Total[c.x][c.y];
+        }
+
+        private int[] next(int[] actual)
+        {
+            return solucion[actual[0]][actual[1]];
         }
 
         private void imprimirArreglo()
@@ -236,110 +321,32 @@ namespace Proyecto_IA1.Mapa
             }
         }
 
-        private bool todosVisitados(int[] visitados)
+
+        static void Main(string[] args)
         {
-            for (int i = 0; i < visitados.Length; i++)
-            {
-                if (visitados[i] == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private bool existeSolucion()
-        {
-            int[] indice = new int[2];
-            indice[0] = inicioX;
-            indice[1] = inicioY;
-
-            bool listo = false;
-            while (!listo)
-            {
-                if (indice[0] == this.finalX && indice[1] == this.finalY)
-                {
-                    listo = true;
-                }
-                if(indice[0] == -1)
-                {
-                    return false;
-                }
-                indice = next(indice);
-            }
-            return true;
-        }
-
-        public ArrayList demeSolucion()
-        {
-            ArrayList res = new ArrayList();
-            int[] indice = new int[2];
-            indice[0] = inicioX;
-            indice[1] = inicioY;
-
-            bool listo = false;
-            while (!listo)
-            {
-                if (indice[0] == this.finalX && indice[1] == this.finalY)
-                {
-                    listo = true;
-                }
-                res.Add(indice);
-                indice = next(indice);
-            }
-            return res;
-        }
-
-        private int[] next(int[] actual)
-        {
-            return indiceLista_Matriz(solucion[indiceMatriz_Lista(actual[0], actual[1])]);
-        }
-
-        static void Main2()
-        {
-            Mapa m = new Mapa(5, 6, 100);
+            Mapa m = new Mapa(6,6,100);
             m.colocarInicio(0, 0);
-            m.colocarFinal(4, 4);
+            m.colocarFinal(5, 5);
             m.colocarObstaculos(10);
-            System.Console.WriteLine("-----------------------------------------------------------------");
-            System.Console.WriteLine("El mapa actual");
-            System.Console.WriteLine("-----------------------------------------------------------------");
+            m.a_estrella(true);
             m.imprimirArreglo();
-            m.dijkstra();
 
-            
-            if (m.existeSolucion())
+            bool termino = false;
+            int[] actual = new int[2];
+            actual[0] = 0;
+            actual[1] = 0;
+            while (!termino)
             {
-                ArrayList lista = m.demeSolucion();
-                System.Console.WriteLine("El tamaño de la solucion es: " + lista.Count);
-
-                System.Console.WriteLine();
-                System.Console.WriteLine("-----------------------------------------------------------------");
-                System.Console.WriteLine("La solucion es:");
-                System.Console.WriteLine("-----------------------------------------------------------------");
-                System.Console.WriteLine("X Y");
-
-                foreach (int[] n in lista)
+                System.Console.WriteLine(actual[0] + " " + actual[1]);
+                if(actual[0] == 5 && actual[1] == 5)
                 {
-                    System.Console.WriteLine(n[0] + " " + n[1]);
-
+                    termino = true;
                 }
-            }else
-            {
-                System.Console.WriteLine("No existe solucion.");
+                actual = m.next(actual);
+
             }
-            
-            
-
-            
-            
-            
-
 
             System.Console.Read();
         }
-
-
-
-    }
+    } 
 }
